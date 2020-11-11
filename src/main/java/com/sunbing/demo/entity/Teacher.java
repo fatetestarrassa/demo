0978d9135ca64d3e6a071622c30490ed7cf255b9
sunbing <805166807@qq.com>
2020-11-11 15:40:45 +0800

    first commit

---
+++ src/main/java/com/sunbing/demo/entity/Teacher.java
@@ -0,0 +1,14 @@
+package com.sunbing.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author sunbing
+ * @version 1.0
+ * @since 2020/11/11 13:47
+ */
+@Data
+public class Teacher {
+    private Integer id;
+    private String name;
+}
---
+++ src/main/java/com/sunbing/demo/mapper/TeacherMapper.java
@@ -0,0 +1,13 @@
+package com.sunbing.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sunbing.demo.entity.Teacher;
+
+/**
+ * @author sunbing
+ * @version 1.0
+ * @since 2020/11/11 13:48
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
